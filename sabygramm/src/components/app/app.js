91aeac76933b77685125ed59762533cf7fa03d62
Anzhelina <anzhelina.gafurova@gmail.com>
2021-07-21 15:06:19 +0500

    test web socket

--- sabygramm/src/components/app/app.js
+++ sabygramm/src/components/app/app.js
@@ -5,31 +5,9 @@ import Edit from '../pages/edit/edit';
 import Dialogs from '../pages/dialogs/dialogs';
 import ChatApp from '../chatApp/index'
 import Contacts from '../pages/contacts/contacts';
+import TestWebSocket from '../test/test';
 
-let socket = new WebSocket("wss://javascript.info/article/websocket/demo/hello");
-socket.onopen = function(e) {
-    alert("[open] Соединение установлено");
-    alert("Отправляем данные на сервер");
-    socket.send("Меня зовут Анжелина");
-  };
-  
-  socket.onmessage = function(event) {
-    alert(`[message] Данные получены с сервера: ${event.data}`);
-  };
-  
-  socket.onclose = function(event) {
-    if (event.wasClean) {
-      alert(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
-    } else {
-      // например, сервер убил процесс или сеть недоступна
-      // обычно в этом случае event.code 1006
-      alert('[close] Соединение прервано');
-    }
-  };
-  
-  socket.onerror = function(error) {
-    alert(`[error] ${error.message}`);
-  };
+
 const App = () => {
     return (
         <Switch>
@@ -37,6 +15,11 @@ const App = () => {
             <Route path="/edit" component={Edit} />
             <Route path="/dialogs" component={Dialogs} />
             <Route path="/contacts" component={Contacts} />
+            <Route path="/test" render={
+              () => {
+                return <TestWebSocket socket={new WebSocket("wss://javascript.info/article/websocket/chat/ws")}/>
+              }
+            } />
             <Route path="/chatapp/:id"
                 render={({ match }) => {
                     const { id, groupId } = match.params;
