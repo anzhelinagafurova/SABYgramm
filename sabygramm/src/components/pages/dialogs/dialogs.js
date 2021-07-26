dbd3ea57d5f752c57bf15698f815ca3b184f2af1
ksusa <k.ero00@mail.ru>
2021-07-26 11:34:31 +0500

    id_pair

--- sabygramm/src/components/pages/dialogs/dialogs.js
+++ sabygramm/src/components/pages/dialogs/dialogs.js
@@ -18,14 +18,14 @@ export default class Dialogs extends Component {
     }
 
     componentDidMount() {
-        // this.service.getDialogData()
-        // .then((dialogs) => dialogs.json())
-        // .then((result) => {
-        //     this.setState({dialogs:result});
-        // }) 
+        this.service.getDialogData()
+            .then((dialogs) => dialogs.json())
+            .then((result) => {
+                this.setState({ dialogs: result });
+            })
 
-        let result = this.service.getMockedData();
-        this.setState({ dialogs: result });
+        // let result = this.service.getMockedData();
+        // this.setState({ dialogs: result });
 
     }
 
@@ -60,25 +60,25 @@ export default class Dialogs extends Component {
             <Swiper className="mySwiper" initialSlide="1" onSlideChange={this.slideChanged}>
                 <Header slot="container-start" groupId={this.state.slideGroup} onSearch={this.renderSearchItems} dialogs={this.state.dialogs} onSettings={this.slideSettings} />
                 <div className="dialog-container">
-            <SwiperSlide>
-                <SettingPage/>
-            </SwiperSlide>
-    
-            <SwiperSlide>
-                {this.addContact()}
-                <RenderDialog dialogs={this.state.dialogs[0]} itemsFound={this.state.itemsFound} groupId={0} />
-            </SwiperSlide>
-    
-            <SwiperSlide>
-                {this.addContact()}
-                <RenderDialog dialogs={this.state.dialogs[1]} itemsFound={this.state.itemsFound} groupId={1} />
-            </SwiperSlide>
-    
-            <SwiperSlide>
-                {this.addContact()}
-                <RenderDialog dialogs={this.state.dialogs[2]} itemsFound={this.state.itemsFound} groupId={2} />
-            </SwiperSlide>
-        </div>
+                    <SwiperSlide>
+                        <SettingPage />
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        {this.addContact()}
+                        <RenderDialog dialogs={this.state.dialogs[0]} itemsFound={this.state.itemsFound} groupId={0} />
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        {this.addContact()}
+                        <RenderDialog dialogs={this.state.dialogs[1]} itemsFound={this.state.itemsFound} groupId={1} />
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        {this.addContact()}
+                        <RenderDialog dialogs={this.state.dialogs[2]} itemsFound={this.state.itemsFound} groupId={2} />
+                    </SwiperSlide>
+                </div>
             </Swiper>
         )
     }
--- sabygramm/src/services/SabygramService.js
+++ sabygramm/src/services/SabygramService.js
@@ -3,6 +3,7 @@ export default class SabygramService {
         [
             {
                 id: 0,
+                id_pair: 1,
                 name: "Анжелина",
                 lastMessage: "Как ты?",
                 img: "https://bugaga.ru/uploads/posts/2016-12/1481556556_kot-volshebnik-1.jpg",
@@ -10,6 +11,7 @@ export default class SabygramService {
             },
             {
                 id: 1,
+                id_pair: 2,
                 name: "Крис",
                 lastMessage: "Встала сегодня в 6 утра прикинь...",
                 img: "https://yt3.ggpht.com/a/AATXAJynSQVjxLr742pi_eZXJxyzjfWLaRcCxgYaeJX_Cg=s900-c-k-c0xffffffff-no-rj-mo",
@@ -21,6 +23,7 @@ export default class SabygramService {
 
             {
                 id: 2,
+                id_pair: 3,
                 name: "Сергей",
                 lastMessage: "Не забывайте подписывать время в тасках",
                 img: "https://i02.fotocdn.net/s110/defbcd4b451a9263/gallery_xl/2460844912.jpg",
@@ -28,6 +31,7 @@ export default class SabygramService {
             },
             {
                 id: 3,
+                id_pair: 4,
                 name: "Эдуард",
                 lastMessage: "Пароль не подошел",
                 img: "https://pm1.narvii.com/7357/dd8ddd1a25b83dcf20d7036a4656fc47b7aad917r1-1080-1198v2_hq.jpg",
@@ -35,6 +39,7 @@ export default class SabygramService {
             },
             {
                 id: 7,
+                id_pair: 5,
                 name: "Серёжа",
                 lastMessage: "Привет, у меня вопрос",
                 img: "https://i0.wp.com/prostolike.net/wp-content/uploads/2018/08/image3_700x669.jpg?w=700&ssl=1",
@@ -44,6 +49,7 @@ export default class SabygramService {
         [
             {
                 id: 4,
+                id_pair: 6,
                 name: "Вова",
                 lastMessage: "Выучил реакт за час",
                 img: "https://cutewallpaper.org/21/grumpy-cat-screensaver/Grumpy-Wallpapers-Wallpaper-Cave.jpg",
@@ -51,6 +57,7 @@ export default class SabygramService {
             },
             {
                 id: 5,
+                id_pair: 7,
                 name: "Саша",
                 lastMessage: "Я на 2 этаже куда идти?",
                 img: "https://ustaliy.ru/wp-content/uploads/2019/03/universe.jpg",
@@ -58,6 +65,7 @@ export default class SabygramService {
             },
             {
                 id: 6,
+                id_pair: 8,
                 name: "Лёша",
                 lastMessage: "Я в столовой, если что",
                 img: "https://avatars.mds.yandex.net/get-zen_doc/1886729/pub_5d62dba6998ed600ad85c10e_5d62dbc135c8d800adef2383/scale_1200",
@@ -66,7 +74,7 @@ export default class SabygramService {
 
         ]
     ];
-    getMockedData(){
+    getMockedData() {
         return this.dialogData;
     }
     sendDataPost(data, url) {
@@ -88,7 +96,7 @@ export default class SabygramService {
         })
     }
     sendDataGet(data, url) {
-        
+
         // return fetch(url, {
         //     method: 'GET',
         //     headers: {
