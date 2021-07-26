9ae61adf51c3782dd435b5c724de9166068553df
ksusa <k.ero00@mail.ru>
2021-07-26 14:34:55 +0500

    build

--- sabygramm/src/components/chatMenu/chatMenu.js
+++ sabygramm/src/components/chatMenu/chatMenu.js
@@ -3,12 +3,12 @@ import './chatMenu.scss';
 import SabygramService from '../../services/SabygramService';
 
 
-const ChatMenu = ({ display, groupId, id, margin }) => {
+const ChatMenu = ({ display, groupId, id, margin, canChange }) => {
 
   const service = new SabygramService();
 
   const onHandleClicked = (id, group_number) => {
-    if (display === 'block') {
+    if (canChange) {
       service.handleDialogs({
         user_id: id,
         group_number: group_number
--- sabygramm/src/components/pages/cinema/cinema.js
+++ sabygramm/src/components/pages/cinema/cinema.js
@@ -16,7 +16,7 @@ export default class Cinema extends Component {
   render() {
     return (
       <div className='cinema-page'>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>>
+        <iframe className="movie" src={this.state.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div className='blue-div-link'>
 
           <input className='link' value="https://www.youtube.com/embed/dQw4w9WgXcQ" placeholder='https://www.youtube.com/embed/dQw4w9WgXcQ' onChange={this.changeLink} />
