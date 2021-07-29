71d233177b2d0e10ce0fd5a3f492a8a0e4f6a1d7
Anzhelina <anzhelina.gafurova@gmail.com>
2021-07-29 13:28:41 +0500

    redux-persist

--- sabygramm/src/index.js
+++ sabygramm/src/index.js
@@ -4,14 +4,18 @@ import App from './components/app';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './store';
+
+import {store, persistor} from './store';
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <Router>
         <App />
       </Router>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
